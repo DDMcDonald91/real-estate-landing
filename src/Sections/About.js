2a5dcc11c3e0ed3db7ab1be214a8aca7d2902ec6
D. McDonald <d.mcdonald@MACs-MBP.attlocal.net>
2022-08-17 12:51:42 -0500

    ui update

--- src/Sections/About.js
+++ src/Sections/About.js
@@ -1,16 +1,19 @@
 import { Container, Row, Col } from 'react-bootstrap'
 import DisplayCard from './Components/DisplayCard'
+import house8 from '../Images/house8.jpg'
+import house6 from '../Images/house6.jpg'
+import house7 from '../Images/house4.jpg'
 
 
 export default function About() {
   return (
     <Container id='about-container'>
         <h1>Explore Black Estates Realty</h1>
+        <p>Learn more about the best agent in the area!</p>
         <Row>
-            <Col sm={12} md={3}><DisplayCard /></Col>
-            <Col sm={12} md={3}><DisplayCard /></Col>
-            <Col sm={12} md={3}><DisplayCard /></Col>
-            <Col sm={12} md={3}><DisplayCard /></Col>
+            <Col sm={12} md={4}><DisplayCard image={house8} /></Col>
+            <Col sm={12} md={4}><DisplayCard image={house7}/></Col>
+            <Col sm={12} md={4}><DisplayCard image={house6}/></Col>
         </Row>
     </Container>
   )
--- src/Sections/Components/DisplayCard.js
+++ src/Sections/Components/DisplayCard.js
@@ -1,12 +1,11 @@
 import { Container, Card } from 'react-bootstrap'
-import  hero  from '../../Images/hero.jpg'
 
-export default function DisplayCard() {
+export default function DisplayCard(props) {
   return (
     <Container style={{marginLeft: 'auto', marginRight: 'auto'}}>
         <Card style={{ maxWidth: '18rem', padding: '0', marginLeft: 'auto', marginRight: 'auto'}}>
             <Card.Body id='card-body'>
-            <Card.Img variant="top" src={hero} className='img-fluid card-image' />
+            <Card.Img variant="top" src={props.image} className='img-fluid card-image' />
             </Card.Body>
         </Card>
     </Container>
